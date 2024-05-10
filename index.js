554ac9924e4ae45e359c49fd9c94b2aa2d403f9c
siam02 <siam.jaman02@gmail.com>
2024-05-10 20:00:54 +0600

    Book delete API route added

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
             res.send({ count });
         })
 
+        app.delete('/book/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bookCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
