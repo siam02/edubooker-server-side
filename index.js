90fc232ca511708c9aa220da2abb8b3d39f8d570
siam02 <siam.jaman02@gmail.com>
2024-05-11 15:12:20 +0600

    Get Single Category API added

--- index.js
+++ index.js
@@ -109,6 +109,13 @@ async function run() {
             res.send(categories);
         })
 
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await categoryCollection.findOne(query);
+            res.send(result);
+        })
+
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
